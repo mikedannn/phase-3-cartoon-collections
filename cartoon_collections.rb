74134bbc43ed597eb1711ab1d9cd4a0ec680a040
Michael <michael.daniels1013@gmail.com>
2022-09-09 00:03:51 -0500

    commit tree 572ba7249a09e671214cbccf991e8302deabcea4

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.map.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map do |call|
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+def find_the_cheese(food_items)
+  cheeses = %w[gouda cheddar camembert]
+  food_items.find do |food|
+    cheeses.include?(food)
+  end
+ end
